8ef869ce3e4d9b518253871bf325bd6437940c9c
LordMark <mv20040908@gmail.com>
2023-05-12 18:08:37 -0300

    Banco de dados funcionando

---
+++ filmezandoPrincipal/index.php
@@ -0,0 +1,124 @@
+<?php include("cabecalho.php"); ?>
+
+
+
+<div class="banner">
+
+
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="bannee2">
+        <div class="carousel-item active">
+          <img class="d-block w-90" src="images/ggg.jpg" alt="First slide">
+        </div>
+
+        <div class="carousel-item">
+          <img class="d-block w-90" src="images/doreto.jpg" alt="Second slide">
+
+        </div>
+        
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+
+
+    </div>
+
+  </div>
+
+            <h1 class="title">FILMES</h1>
+<p></p>
+<div class="p">
+<a href='http://url-destino'><p2>#Críticas </p2></a> <a href='http://url-destino'><p3> #Top melhores 2022 </p3></a> <a href='http://url-destino'> <p5>#Em cartaz</p5></a>
+</div>
+
+  
+</div>
+<h4 class="sub">MAIS ESPERADOS</h4>
+
+  <div class="row">
+    <div class="item ">
+      <a href='http://url-destino'> <img class="arredondado" src="images/flash_ver3.jpg" class="img-fluid"
+          alt="Responsive image"> </a> 
+          <div class="intro"><p>Flash</p></div>
+          
+    </div>
+    <div class="item ">
+      <a href='http://url-destino'><img class="arredondado" src="images/oppenheimer_2.jpg" class="img-fluid"
+          alt="Responsive image"></a>
+          <p>Oppenheimer</p>
+          
+
+    </div>
+      <div class="item">
+        
+        <a href='http://url-destino'><img class="arredondado" src="images/spiderman_across_the_spiderverse_ver3.jpg"
+            class="img-fluid" alt="Responsive image"></a> 
+            <p>Homem-aranha <br> Através do Aranha verso</p>
+</div>
+
+            <div class="item">
+        <a href='http://url-destino'> <img class="arredondado" src="images/transformers_rise_of_the_beasts_ver11_xlg.jpg  "
+            class="img-fluid" alt="Responsive image"></a>
+        <p>Transformers O Despertar das Feras</p>
+      </div>
+
+      <div class="item">
+        <a href='http://url-destino'> <img class="arredondado" src="images/indiana jones.jpg  "
+            class="img-fluid" alt="Responsive image"></a>
+        <p>Indiana Jones: A relíquia do destino</p>
+      </div>
+
+      <div class="item">
+        <a href='http://url-destino'> <img class="arredondado" src="images/barbie.jpg "
+            class="img-fluid" alt="Responsive image"></a>
+        <p>Barbie</p>
+      </div>
+
+      <div class="item">
+        <a href='http://url-destino'> <img class="arredondado" src="images/marvels_xlg.jpg"
+            class="img-fluid" alt="Responsive image"></a>
+        <p>As Marvels</p>
+      </div>
+
+      
+      <div class="item">
+        <a href='http://url-destino'> <img class="arredondado" src="images/missao impssivel.jpg"
+            class="img-fluid" alt="Responsive image"></a>
+        <p>Missão: Impossível – Acerto de Contas </p>
+      </div>
+
+      
+      <div class="item">
+        <a href='http://url-destino'> <img class="arredondado" src="images/fastxposter_3955893.jpg"
+            class="img-fluid" alt="Responsive image"></a>
+        <p>Velozes e Furiosos 10</p>
+      </div>
+
+      <div class="item">
+        <a href='http://url-destino'> <img class="arredondado" src="images/besouro azul.jpg"
+            class="img-fluid" alt="Responsive image"></a>
+        <p>Besouro Azul</p>
+      </div>
+</div>
+
+
+      
+        
+            
+
+
+
+
+ <?php include("rodape.php"); ?>
