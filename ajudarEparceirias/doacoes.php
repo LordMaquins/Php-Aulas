0867c14913a570f7f9594c9b0149192400258686
LordMark <mv20040908@gmail.com>
2023-09-05 18:17:10 -0300

    Pagina de doacao, falta o css

---
+++ ajudarEparceirias/doacoes.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quero ajudar</title>
+</head>
+<body>
+    <h1>Quero Ajudar</h1>
+    <p>Junte-se a nós na missão de fazer a diferença na vida de animais abandonados e resgatados!<br>
+     Nossa ONG é apaixonada por encontrar lares amorosos para esses aumigos peludos que merecem <br> uma segunda chance. <br>
+     Cada um deles é entregue a você castrado, vacinado e livre de vermes, prontos para se tornarem seus fiéis amigos.</p>
+
+    <h1>Salve Vidas Peludas</h1>  
+    <!-- Se quiser colocar uma imagem coloque aqui -->
+
+    <h3>Junte-se à Nossa Missão</h3>
+
+    <p>Você já sentiu o amor incondicional de um animal de estimação? <br>
+    Agora é a sua chance de experimentar essa conexão especial. <br>
+    Com sua generosa doação, podemos continuar a proporcionar <br>
+    cuidados de qualidade a esses aumiguinhos de quatro patas e ajudá-los <br>
+     a encontrar o amor e o carinho que tanto merecem.
+</p>
+
+<h2>1- Doações via Pix ou transferência</h2>
+<p>A cada 25 reais doados, voce ajuda 5 aumigos. <br>
+Para doar você pode fazer uma transferência Bancária ou Pix.
+</p>
+<h2> Pix:</h2>
+ <h3>email: "contato.auqimia@gmail.com"</h3>
+
+<h2>2-TED/depósito em conta</h2> 
+<h3>
+    Banco "" <br>
+    Agência "" <br>
+    Conta-corrente "" <br>
+    Ong Auqimia <br>
+    CNPJ ""
+</h3>
+
+<h4>"Adote um aumigo peludo e juntos construiremos uma história de amor inesquecível."</h4>
+    </body>
+</html>
